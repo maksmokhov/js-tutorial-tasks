49f4efecef9b3912bed37c8cc2b03628f2a33210
maksmokhov <mencaurara@gmail.com>
2022-10-14 00:32:21 +0300

    variables

---
+++ variables/03-uppercase-or-lowercase/03.js
@@ -0,0 +1,4 @@
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+const BIRTHDAY = '18.04.1982'; // Можно, потому что здесь значение сохраняется
+const AGE = someCode(BIRTHDAY); // Нельзя, здесь значени "плавает"
